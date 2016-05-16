389e3c50df563a94a85c6d6a5054b62a232b4f1c
Nicholas Alan Brown <nicholasalanbrown@gmail.com>
2016-05-16 12:27:00 -0400

    Simple NavigatorIOS example

---
+++ application/components/Landing.js
@@ -0,0 +1,43 @@
+import React, {
+  Component,
+} from 'react';
+
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+
+import Dashboard from './Dashboard';
+
+export default class Landing extends Component{
+  render(){
+    return (
+      <View style={styles.container}>
+        <Text style={styles.h1}>This is Landing</Text>
+        <TouchableOpacity onPress={() => {
+          this.props.navigator.push({
+            title: 'Dashboard',
+            component: Dashboard,
+          });
+        }}>
+          <Text>Go to Dashboard</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+};
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  h1: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    padding: 20,
+  },
+});
