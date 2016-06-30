fff577ae852f38feb087f550f35cec433737bc42
tomatagolo <tom@agolo.com>
2016-06-29 20:14:08 -0400

    Flesh out landing page

--- application/components/Landing.js
+++ application/components/Landing.js
@@ -4,48 +4,98 @@ import {
   StyleSheet,
   Text,
   TouchableOpacity,
+  Dimensions,
+  Image,
   View
 } from 'react-native';
 
-import Dashboard from './Dashboard';
 import NavigationBar from 'react-native-navbar';
+import Colors from '../styles/colors';
+
+let { width: deviceWidth, height: deviceHeight } = Dimensions.get('window');
 
 export default class Landing extends Component{
   render(){
     return (
-      <View style={styles.outerContainer}>
-        <NavigationBar
-            title={{title: 'Landing', tintColor: 'white'}}
-            tintColor='#3A7BD2'
-          />
-        <View style={styles.container}>
-          <Text style={styles.h1}>This is the Landing</Text>
-          <TouchableOpacity onPress={() => {
+      <View style={styles.container}>
+        <View style={styles.backgroundHolder}>
+          <Image style={styles.image} source={require('../assets/images/welcome.png')}/>
+        </View>
+        <View style={styles.logoContainer}>
+          <Image style={styles.logo} source={require('../assets/images/logo.png')}/>
+          <Text style={styles.title}>assemblies</Text>
+          <Text style={styles.subTitle}>Where Developers Connect</Text>
+        </View>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
             this.props.navigator.push({
               name: 'Dashboard'
-            });
-          }}>
-            <Text>Go to Dashboard</Text>
-          </TouchableOpacity>
-        </View>
+            })
+          }}
+        >
+          <Text style={styles.buttonText}>Go to Dashboard</Text>
+        </TouchableOpacity>
       </View>
     );
   }
 };
 
 let styles = StyleSheet.create({
-  outerContainer: {
-    flex: 1,
-    backgroundColor: 'white'
+  backgroundHolder: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
   },
-  container: {
-    flex: 1,
+  button: {
+    height: 80,
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    backgroundColor: Colors.brandPrimary,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  h1: {
-    fontSize: 22,
-    fontWeight: 'bold',
-    padding: 20,
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '700',
+  },
+  container: {
+    flex: 1,
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+  },
+  image: {
+    height: deviceHeight,
+    width: deviceWidth,
+  },
+  logo: {
+    height: 90,
+    width: 90,
+  },
+  logoContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 50,
+  },
+  subTitle: {
+    color: 'white',
+    fontSize: 20,
+  },
+  title: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: '700',
+    paddingBottom: 24,
   },
 });
