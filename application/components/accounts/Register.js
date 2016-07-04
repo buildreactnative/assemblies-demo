25d416693236256a5bb1f34a82f3001924833810
tomatagolo <tom@agolo.com>
2016-07-04 14:19:01 -0400

    Add environment variables and Google Places Autocomplete to registration form

--- application/components/accounts/Register.js
+++ application/components/accounts/Register.js
@@ -4,6 +4,7 @@ import {
   View,
   ScrollView,
   TextInput,
+  StyleSheet,
   TouchableOpacity,
   Dimensions
 } from 'react-native';
@@ -12,7 +13,9 @@ import NavigationBar from 'react-native-navbar';
 import Colors from '../../styles/colors';
 import Globals from '../../styles/globals';
 import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+import _ from 'underscore';
 import {DEV} from '../../config';
+import Config from 'react-native-config';
 
 const { width: deviceWidth, height: deviceHeight } = Dimensions.get('window');
 import LeftButton from './LeftButton';
@@ -35,18 +38,196 @@ class Register extends Component{
     return (
       <View style={styles.container}>
         <NavigationBar title={titleConfig} tintColor={Colors.brandPrimary} leftButton={<LeftButton navigator={navigator}/>} />
-        <ScrollView>
+        <ScrollView style={styles.formContainer}>
+          <TouchableOpacity onPress={()=> navigator.push({ name: 'Login' })}>
+            <Text style={styles.h5}>
+              Already have an account? <Text style={styles.technologyList}>Login</Text>
+            </Text>
+          </TouchableOpacity>
+          <Text style={styles.h4}>{"* Where are you looking for assemblies?"}</Text>
+          <View ref="location" style={{flex: 1,}}>
+            <GooglePlacesAutocomplete
+              styles={autocompleteStyles}
+              placeholder='Your city'
+              minLength={2}
+              autoFocus={false}
+              fetchDetails={true}
+              onPress={(data, details = null) => { // 'details' is provided when fetchDetails = true
+                if (DEV) {console.log(data);}
+                if (DEV) {console.log(details);}
+                this.setState({
+                  location: _.extend({}, details.geometry.location, {
+                    city: _.find(details.address_components, (c) => c.types[0] == 'locality'),
+                    state: _.find(details.address_components, (c) => c.types[0] == 'administrative_area_level_1'),
+                    county: _.find(details.address_components, (c) => c.types[0] == 'administrative_area_level_2'),
+                    formattedAddress: details.formatted_address,
+                  })
+                });
+              }}
+              getDefaultValue={() => {return '';}}
+              query={{
+                key       :  Config.GOOGLE_PLACES_API_KEY,
+                language  : 'en', // language of the results
+                types     : '(cities)', // default: 'geocode'
+              }}
+              currentLocation={false}
+              currentLocationLabel="Current location"
+              nearbyPlacesAPI='GooglePlacesSearch'
+              GoogleReverseGeocodingQuery={{}}
+              GooglePlacesSearchQuery={{rankby: 'distance',}}
+              filterReverseGeocodingByTypes={['street_address']} // filter the reverse geocoding results by types - ['locality', 'administrative_area_level_3'] if you want to display only cities
+              predefinedPlaces={[]}>
+            </GooglePlacesAutocomplete>
+          </View>
         </ScrollView>
       </View>
     )
   }
 }
 
-let styles = {
+
+let styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backButton: {
+    paddingLeft: 20,
+    backgroundColor: 'transparent',
+    paddingBottom: 10,
+  },
+  technologyList:{
+    textAlign: 'left',
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: Colors.brandPrimary,
+    paddingHorizontal: 20,
+    marginLeft: 8,
+    paddingVertical: 4,
+  },
+  formContainer: {
+    backgroundColor: Colors.inactive,
+    flex: 1,
+    paddingTop: 15,
+  },
+  contentContainerStyle: {
+    flex: 1,
+  },
+  h4: {
+    fontSize: 20,
+    fontWeight: '300',
+    color: 'black',
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  h5: {
+    fontSize: 16,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    textAlign: 'center',
+  },
+  formField: {
+    backgroundColor: 'white',
+    height: 50,
+    paddingTop: 5,
+    marginBottom: 10,
+  },
+  largeFormField: {
+    backgroundColor: 'white',
+    height: 100,
+  },
+  addPhotoContainer: {
+    backgroundColor: 'white',
+    marginVertical: 15,
+      marginHorizontal: (deviceWidth - 200) / 2,
+    width: 200,
+    borderRadius: 30,
+    paddingVertical: 15,
+    paddingHorizontal: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  photoText: {
+    fontSize: 18,
+    paddingHorizontal: 10,
+    color: Colors.brandPrimary
+  },
+  input: {
+    color: '#777',
+    fontSize: 18,
+    fontWeight: '300',
+    height: 40,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+  pb: {
+    paddingBottom: 10,
+  },
+  largeInput: {
+    color: '#ccc',
+    fontSize: 18,
+    backgroundColor: 'white',
+    fontWeight: '300',
+    height: 100,
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+  },
+});
+
+const autocompleteStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'white',
-  }
-}
+  },
+  textInputContainer: {
+    backgroundColor: 'white',
+    height: 44,
+    borderTopColor: 'white',
+    borderBottomColor: 'white',
+  },
+  textInput: {
+    backgroundColor: 'white',
+    height: 28,
+    borderRadius: 5,
+    paddingTop: 4.5,
+    paddingBottom: 4.5,
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginTop: 7.5,
+    marginLeft: 8,
+    marginRight: 8,
+    fontSize: 18,
+  },
+  poweredContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.inactive,
+  },
+  powered: {
+    marginTop: 15,
+  },
+  listView: {
+    // flex: 1,
+  },
+  row: {
+    padding: 13,
+    height: 44,
+    flexDirection: 'row',
+  },
+  separator: {
+    height: 1,
+    backgroundColor: 'white',
+  },
+  description: {
+  },
+  loader: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    height: 20,
+  },
+  androidLoader: {
+    marginRight: -15,
+  },
+});
 
 export default Register;
